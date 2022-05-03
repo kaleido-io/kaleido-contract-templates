5b2f840ab107f6e164cbc8bd8721fa6969a3323b
whitefeather2772 <103925730+whitefeather2772@users.noreply.github.com>
2022-05-02 21:21:50 -0500

    Add files via upload

---
+++ Draconian.sol erc721.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact whitefeather@whitebuffalotribe.onmicrosoft.com
+contract Draconian is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
+    constructor() ERC721("Draconian", "DGN") {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
