62441123b93266a5e1d8e14a4a3b1772d5ea60af
Galen Keene <galen.keene@kaleido.io>
2023-07-12 21:17:28 -0400

    roll back firefly 1.2 contracts, add docker build templates for chaincodes

--- contracts/firefly/1.2.0/Firefly.sol
+++ contracts/firefly/1.2.0/Firefly.sol
@@ -2,57 +2,25 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
-import "./IBatchPin.sol";
+contract Firefly {
 
-contract Firefly is IBatchPin {
-    function pinBatchData(bytes calldata data) public override {
-        bytes32 uuids;
-        bytes32 batchHash;
-        string memory payloadRef;
-        bytes32[] memory contexts;
-        (uuids, batchHash, payloadRef, contexts) = abi.decode(
-            data,
-            (bytes32, bytes32, string, bytes32[])
-        );
-        emit BatchPin(
-            tx.origin,
-            block.timestamp,
-            "firefly:contract_invoke_pin",
-            uuids,
-            batchHash,
-            payloadRef,
-            contexts
-        );
-    }
-
-    function pinBatch(
+    event BatchPin (
+        address author,
+        uint timestamp,
+        string action,
         bytes32 uuids,
         bytes32 batchHash,
-        string memory payloadRef,
-        bytes32[] memory contexts
-    ) public override {
-        emit BatchPin(
-            tx.origin,
-            block.timestamp,
-            "firefly:batch_pin",
-            uuids,
-            batchHash,
-            payloadRef,
-            contexts
-        );
+        string payloadRef,
+        bytes32[] contexts
+    );
+
+    function pinBatch(bytes32 uuids, bytes32 batchHash, string memory payloadRef, bytes32[] memory contexts) public {
+        emit BatchPin(msg.sender, block.timestamp, "", uuids, batchHash, payloadRef, contexts);
     }
 
     function networkAction(string memory action, string memory payload) public {
         bytes32[] memory contexts;
-        emit BatchPin(
-            tx.origin,
-            block.timestamp,
-            action,
-            0,
-            0,
-            payload,
-            contexts
-        );
+        emit BatchPin(msg.sender, block.timestamp, action, 0, 0, payload, contexts);
     }
 
     function networkVersion() public pure returns (uint8) {
